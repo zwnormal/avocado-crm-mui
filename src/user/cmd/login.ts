00bc24b654dc4c5b592ec1bcf94eaa7e7fe4f72a
Wei Zheng <zwnormal@gmail.com>
2023-10-13 09:35:20 +1100

    Login accept cookie from the server

--- src/user/cmd/login.ts
+++ src/user/cmd/login.ts
@@ -1,5 +1,3 @@
-import Cookies from "universal-cookie";
-
 export default async function login (email: string, password: string) {
     const response = await fetch("http://127.0.0.1:3000/api/user/login", {
         method: "POST",
@@ -7,12 +5,9 @@ export default async function login (email: string, password: string) {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({email, password}),
+        credentials: 'include',
     });
-    if (response.ok) {
-        const session = await response.json();
-        const cookies = new Cookies(null, { path: '/' });
-        cookies.set("session_id", session.session_id);
-    } else {
-        throw new Error(await response.text())
+    if (!response.ok) {
+        throw new Error(await response.text());
     }
 }
